e9ad5da224a998ae784d0ff15fc514f54248af19
Sophia Wallace <47252820+WallaceRepo@users.noreply.github.com>
2023-04-18 20:29:05 -0500

    Update MethodChallenge.java

--- src/MethodChallenge.java
+++ src/MethodChallenge.java
@@ -34,3 +34,27 @@ public class MethodChallenge {
         return getDurationString(result);
     }
 }
+
+
+
+//////////////////////////
+public class MethodChallenge {
+    public static void main(String[] args) {
+
+      System.out.println(getDurationString(-3945));
+      System.out.println(getDurationString(-65, 45));
+      System.out.println(getDurationString(65, 145));
+        }
+    public static String getDurationString (int seconds){
+        if(seconds < 0) return  "Invalid data for seconds(" + seconds + "), must be a positive integer value";
+       int minutes = seconds / 60;
+       return getDurationString(seconds / 60, seconds % 60);
+    }
+    public static String getDurationString (int minutes, int seconds) {
+        if(minutes < 0) return  "Invalid data for minutes(" + minutes + "), must be a positive integer value";
+        if(seconds <= 0 || seconds >=59) return  "Invalid data for seconds(" + seconds + "), must be between 0 and 59";
+        int hours = minutes / 60;
+        int remainingMin = minutes % 60;
+        return hours + "h" +remainingMin + "m " + seconds + "s";
+    }
+}

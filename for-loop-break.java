bbddb0e0b1a4e9eb5c8e847e3cfad9e321337d03
Sophia Wallace <47252820+WallaceRepo@users.noreply.github.com>
2023-05-01 11:03:55 -0500

    Update for-loop-break.java

--- for-loop-break.java
+++ for-loop-break.java
@@ -47,3 +47,20 @@ public class primeNumber {
         return count;
     }
 }
+//// for loop challenge
+
+public class ForLoopChallenge {
+    public static void main(String[] args) {
+        for (double rate = 7.5; rate <=10; rate+=0.25){
+            double interestAmount = calculateInterest(100.0, rate);
+            if(interestAmount > 8.5){
+                break;
+            }
+            System.out.println("10 000 at " + rate + " % interest = " + interestAmount);
+        }
+   }
+    public static double calculateInterest(double amount, double interestRate){
+        return ( amount * (interestRate / 100));
+    }
+
+}

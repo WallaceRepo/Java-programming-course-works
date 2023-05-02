a56b4b35bb95c328428f53689d1cd75bc75f9823
Sophia Wallace <47252820+WallaceRepo@users.noreply.github.com>
2023-05-02 14:46:00 -0500

    Update while-loop-chall-Palindrome.java

--- while-loop-chall-Palindrome.java
+++ while-loop-chall-Palindrome.java
@@ -1,9 +1,10 @@
 public class whileLoopChallenge {
     public static void main(String[] args) {
-        isPalindrome(345);
-
+        isPalindrome(101);
+        System.out.println(isPalindrome(101));
     }
      public static boolean isPalindrome(int number) {
+          int original = number; // number gets changed after while loop!
           int reverse = 0;
           while (number > 0) {
               int lastDigit = number % 10;
@@ -11,9 +12,9 @@ public class whileLoopChallenge {
                  reverse *=10; // every repeat it gets multiplied by 10
                  number /=10; // removing the last digit of number
             }
-          reverse /=10; // to remove extra 0 of end
-          System.out.println(reverse);
-          if(reverse == number) return true;
-        return false;  
+           reverse /=10;
+          System.out.print(reverse);
+          if(reverse == original) return true;
+        return false;
      }
 }

3995283139bdd9b23a4403e6aa91d2b6ab8192eb
Sophia Wallace <47252820+WallaceRepo@users.noreply.github.com>
2023-05-21 22:35:28 -0500

    Create rectangle-last-chall.java

---
+++ OOP-class/rectangle-last-chall.java
@@ -0,0 +1,40 @@
+public class Rectangle {
+
+    public static void main(String[] args) {
+
+    }
+    private double width;
+    private double length;
+
+    public Rectangle ( double width, double length) {
+        this.width = width < 0 ? 0 : width;
+        this.length = length < 0 ? 0 : length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+}
+public class Cuboid extends Rectangle {
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        this.height = height < 0 ? 0 : height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+    public double getVolume(){
+        return getArea() * height;
+    }
+}

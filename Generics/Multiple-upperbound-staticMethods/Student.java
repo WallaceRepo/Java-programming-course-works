e7ff4185c2ace31521ac8cf7fd8036f8fb04f401
Sophia Wallace <47252820+WallaceRepo@users.noreply.github.com>
2023-10-03 18:34:46 -0500

    Create Student.java

---
+++ Generics/Multiple-upperbound-staticMethods/Student.java
@@ -0,0 +1,43 @@
+package model;
+import util.QueryItem;
+
+import java.util.Random;
+
+public class Student implements QueryItem {
+    private String name;
+    private String course;
+    private int yearStarted;
+    protected static Random random = new Random();
+    // I've made them static, because I don't want each instance to have this data,
+    //it can be stored with the class instance instead.
+    private static String[] firstName = { "Ann", "Bill", "Cathy", "John", "Sophia"};
+    private static String [] courses = { "Java", "Python", "C++"};
+
+    public Student() {
+        int lastNameIndex = random.nextInt(65, 91);
+        name = firstName[random.nextInt(5)] + " " + (char) lastNameIndex;
+        course = courses[random.nextInt(3)];
+        yearStarted = random.nextInt(2018, 2023);
+    }
+
+    @Override
+    public String toString() {
+        return "%-15s %-15s %d".formatted(name, course, yearStarted);
+    }
+
+    public int getYearStarted() {
+        return yearStarted;
+    }
+
+    @Override
+    public boolean matchFieldValue(String fieldName, String value) {
+        String fName = fieldName.toUpperCase();
+        return switch(fName) {
+            case "NAME" -> name.equalsIgnoreCase(value); // it means match is found if name and value is equal
+            case "COURSE" -> course.equalsIgnoreCase(value);
+            case "YEARSTARTED" -> yearStarted == (Integer.parseInt(value));
+            // if the course field's value is equal to what the user is trying to match on, it will return true.
+            default -> false;
+        };
+    }
+}

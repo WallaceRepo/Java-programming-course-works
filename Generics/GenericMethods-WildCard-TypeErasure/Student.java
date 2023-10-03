6761ccd4b9a0229ccc2f28207ed11531be49da9e
Sophia Wallace <47252820+WallaceRepo@users.noreply.github.com>
2023-10-03 15:26:26 -0500

    Create Student.java

---
+++ Generics/GenericMethods-WildCard-TypeErasure/Student.java
@@ -0,0 +1,28 @@
+import java.util.Random;
+
+public class Student {
+    private String name;
+    private String course;
+    private int yearStarted;
+    protected static Random random = new Random();
+    // I've made them static, because I don't want each instance to have this data,
+    //it can be stored with the class instance instead.
+    private static String[] firstName = { "Ann", "Bill", "Cathy", "John", "Sophia"};
+    private static String [] courses = { "Java", "Python", "C++"};
+
+    public Student() {
+        int lastNameIndex = random.nextInt(65, 91);
+        name = firstName[random.nextInt(5)] + " " + (char) lastNameIndex;
+        course = courses[random.nextInt(3)];
+        yearStarted = random.nextInt(2018, 2023);
+    }
+
+    @Override
+    public String toString() {
+        return "%-15s %-15s %d".formatted(name, course, yearStarted);
+    }
+
+    public int getYearStarted() {
+        return yearStarted;
+    }
+}

b6b0816600e0a3169d4692a227b3671cd1677054
Sophia Wallace <47252820+WallaceRepo@users.noreply.github.com>
2024-09-27 21:40:28 -0500

    Update 343.Thread-Safe List_Queues_ArrayBlockingQueue.java

--- Thread_Concurency/343.Thread-Safe List_Queues_ArrayBlockingQueue.java
+++ Thread_Concurency/343.Thread-Safe List_Queues_ArrayBlockingQueue.java
@@ -108,3 +108,53 @@ Norton, Bob, (99)
 Norton, Bob, (52)
 Peterson, Bob, (69)
  */
+
+
+// Main.java
+package SchedulingTasks;
+
+
+import java.util.Random;
+import java.util.concurrent.ConcurrentSkipListMap;
+import java.util.stream.Stream;
+record Person(String firstName, String lastName, int age ) {
+    private final static String[] firsts = { "Able", "Bob", "Charlie", "Donna", "Eve" , "Fred"};
+    private final static String[] lasts = {"Norton", "Peterson", "Quincy", "Richardson", "Smith"};
+    private  final static Random random = new Random();
+    public Person() {
+        this( firsts [ random.nextInt(firsts.length)],
+                lasts [ random.nextInt(lasts.length)],
+                random.nextInt(18, 100));
+    }
+    public String toString() {
+        return "%s, %s, (%d)".formatted(lastName, firstName, age);
+
+    }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        var threadMap = new ConcurrentSkipListMap<String, Long>();
+         var persons = Stream.generate(Person::new)
+                 .limit(10000)
+                 .parallel()
+                 .peek((p) -> {
+                     var threadName = Thread.currentThread().getName()
+                             .replace("ForkJoinPool.commonPool-worker-", "thread_");
+                     threadMap.merge(threadName, 1L, Long::sum);
+                 })
+                 .toArray(Person[]::new);
+
+         System.out.println("Total = " + persons.length);
+         System.out.println(threadMap);
+
+         long total = 0;
+         for ( long count : threadMap.values()) {
+             total += count;
+         }
+         System.out.println("ThreadCounts = " + total);
+    }
+}
+
+
+    

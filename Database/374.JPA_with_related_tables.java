bd18757f059ef6c86f75d5925bbbb375300d1df1
Sophia Wallace <47252820+WallaceRepo@users.noreply.github.com>
2024-11-07 10:40:39 -0600

    Create 374.JPA_with_related_tables.java

---
+++ Database/374.JPA_with_related_tables.java
@@ -0,0 +1,174 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import music.Artist;
+// File structure
+/*
+
+/src /main
+/src/META-INF/persistence.xml
+/src/music/Album
+/src/music/Artist
+
+  */
+public class Main {
+    public static void main ( String[] args ) {
+
+         try ( var sessionFactory = Persistence.createEntityManagerFactory(
+                  "dev.lpa.music" );
+               EntityManager entityManager = sessionFactory.createEntityManager();
+         ) {
+             var transaction = entityManager.getTransaction();
+             transaction.begin();
+           //  Artist artist = new Artist(202, "Muddy Water");
+             Artist artist = entityManager.find(Artist.class, 201);
+             System.out.println(artist);
+
+          //   entityManager.remove(artist);
+           //  entityManager.persist( new Artist("Muddy Water"));
+             entityManager.merge(artist);
+             transaction.commit();
+
+         } catch (Exception e ) {
+             e.printStackTrace();
+         }
+    }
+}
+// persistence.xml
+<persistence xmlns="http://java.sun.com/xml/ns/persistence" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://java.sun.com/xml/ns/persistence http://java.sun.com/xml/ns/persistence/persistence_2_0.xsd" version="2.0">
+
+    <persistence-unit name="dev.lpa.music">
+
+        <properties>
+            <property name="jakarta.persistence.jdbc.driver" value="com.mysql.cj.jdbc.Driver"/>
+            <property name="jakarta.persistence.jdbc.url" value="jdbc:mysql://localhost:3306/music"/>
+            <property name="jakarta.persistence.jdbc.user" value="devuser"/>
+            <property name="jakarta.persistence.jdbc.password" value="0802"/>
+            <property name="hibernate.show_sql" value="true" />
+        </properties>
+    </persistence-unit>
+</persistence>
+
+  // Artist.java
+  package music;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "artists")
+public class Artist {
+    @Id
+    @Column (name = "artist_id")
+    private int artistId;
+
+    @Column (name = "artist_name")
+    private String artistName;
+    @OneToMany
+    @JoinColumn(name= "artist_id")
+    private List<Album> albums = new ArrayList<>();
+
+    public Artist() {
+    }
+
+    public Artist(int artistId, String artistName) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+    }
+
+    public List<Album> getAlbums() {
+        return albums;
+    }
+
+    public Artist(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    @Override
+    public String toString() {
+        return "Artist{" +
+                "artistId=" + artistId +
+                ", artistName='" + artistName + '\'' +
+                ", albums=" + albums +
+                '}';
+    }
+}
+// Album.java
+package music;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "albums")
+public class Album {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column (name = "album_id")
+    private int albumId;
+
+    @Column (name = "album_name")
+    private String albumName;
+
+    public Album() {
+    }
+
+    public Album(int albumId, String albumName) {
+        this.albumId = albumId;
+        this.albumName = albumName;
+    }
+
+    public Album(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    @Override
+    public String toString() {
+        return "Album{" +
+                "albumId=" + albumId +
+                ", albumName='" + albumName + '\'' +
+                '}';
+    }
+}
+/*
+Nov 07, 2024 10:18:57 AM org.hibernate.jpa.internal.util.LogHelper logPersistenceUnitInformation
+INFO: HHH000204: Processing PersistenceUnitInfo [name: dev.lpa.music]
+Nov 07, 2024 10:18:57 AM org.hibernate.Version logVersion
+INFO: HHH000412: Hibernate ORM core version 6.6.1.Final
+Nov 07, 2024 10:18:57 AM org.hibernate.cache.internal.RegionFactoryInitiator initiateService
+INFO: HHH000026: Second-level cache disabled
+Nov 07, 2024 10:18:57 AM org.hibernate.engine.jdbc.connections.internal.DriverManagerConnectionProviderImpl configure
+WARN: HHH10001002: Using built-in connection pool (not intended for production use)
+Nov 07, 2024 10:18:58 AM org.hibernate.engine.jdbc.env.internal.JdbcEnvironmentInitiator initiateService
+INFO: HHH10001005: Database info:
+	Database JDBC URL [jdbc:mysql://localhost:3306/music]
+	Database driver: com.mysql.cj.jdbc.Driver
+	Database version: 8.0.39
+	Autocommit mode: false
+	Isolation level: undefined/unknown
+	Minimum pool size: 1
+	Maximum pool size: 20
+Nov 07, 2024 10:18:59 AM org.hibernate.engine.transaction.jta.platform.internal.JtaPlatformInitiator initiateService
+INFO: HHH000489: No JTA platform available (set 'hibernate.transaction.jta.platform' to enable JTA platform integration)
+Hibernate: select a1_0.artist_id,a1_0.artist_name from artists a1_0 where a1_0.artist_id=?
+Hibernate: select a1_0.artist_id,a1_0.album_id,a1_0.album_name from albums a1_0 where a1_0.artist_id=?
+Artist{artistId=201, artistName='Chemical Brothers', albums=[Album{albumId=289, albumName='Push The Button'}, Album{albumId=728, albumName='Push The Button'}]}
+
+*/
